a1883971712a3e785898504f8a40eb29d656f6e9
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-08-07 11:01:33 -0700

    Task 13: Average weighted score for all!

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,13 @@
+-- Create stored procedure ComputeAverageWeightedScoreForUsers
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    UPDATE users u
+    SET average_score = (
+        SELECT IFNULL(SUM(c.score * p.weight) / SUM(p.weight), 0)
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = u.id
+    );
+END //
+DELIMITER ;
