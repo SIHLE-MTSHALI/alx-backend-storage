243308602321ff684cea7a8c5c4b04ef14f88165
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-08-14 11:01:35 -0700

    5. Fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python3
 """
-Web cache and tracker module.
+Implements an expiring web cache and tracker
 """
+
 import redis
 import requests
+from functools import wraps
 from typing import Callable
 
-redis_instance = redis.Redis()
+
+r = redis.Redis()
 
 
+def count_requests(method: Callable) -> Callable:
+    """Decorator to count how many times a request has been made"""
+    @wraps(method)
+    def wrapper(url):
+        """Wrapper function"""
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    """Fetch a page and cache the result in Redis."""
-    count_key = f"count:{url}"
-    redis_instance.incr(count_key)
+    """
+    Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
+    """
+    resp = requests.get(url)
+    return resp.text
 
-    cached_page = redis_instance.get(url)
-    if cached_page:
-        return cached_page.decode('utf-8')
 
-    response = requests.get(url)
-    redis_instance.setex(url, 10, response.text)
-
-    return response.text
+if __name__ == "__main__":
+    url = 'http://slowwly.robertomurray.co.uk'
+    print(get_page(url))
+    print(get_page(url))
+    print(get_page(url))
+    print(r.get(f"count:{url}"))
